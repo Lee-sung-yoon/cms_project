03620bbd29d52a24c8a2ca676686cb98e86634d8
LEE <tjddbs1412@naver.com>
2023-02-17 11:14:12 +0900

    feat : [006-CUSTOMER-BALANCE] 고객에 대한 잔액 조회 구현.
    customer에 대한 잔액 관련 구현.

---
+++ src/main/java/com/example/cms/user/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.example.cms.user.controller;
+
+import com.example.cms.user.domain.customer.ChangeBalanceForm;
+import com.example.cms.user.domain.customer.CustomerDto;
+import com.example.cms.user.domain.model.Customer;
+import com.example.cms.user.exception.CustomException;
+import com.example.cms.user.exception.ErrorCode;
+import com.example.cms.user.service.customer.CustomerBalanceService;
+import com.example.cms.user.service.customer.CustomerService;
+import com.example.domain.common.UserVo;
+import com.example.domain.config.JwtAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final JwtAuthenticationProvider provider;
+    private final CustomerService customerService;
+    private final CustomerBalanceService customerBalanceService;
+
+    @GetMapping("/getInfo")
+    public ResponseEntity<CustomerDto> getInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token) {
+        UserVo vo = provider.getUserVo(token);
+        Customer c = customerService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(
+                () -> new CustomException(ErrorCode.NOT_FOUND_USER)
+        );
+        return ResponseEntity.ok(CustomerDto.from(c));
+    }
+
+    @PostMapping("/balance")
+    public ResponseEntity<Integer> changeBalance(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                 @RequestBody ChangeBalanceForm form) {
+        UserVo vo = provider.getUserVo(token);
+
+        return ResponseEntity.ok(customerBalanceService.changeBalance(vo.getId(), form).getCurrentMoney());
+    }
+
+}
