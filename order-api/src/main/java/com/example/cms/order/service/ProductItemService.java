d32b0c005f58256e7912664e5944e1b75aa2c3d5
LEE <tjddbs1412@naver.com>
2023-02-20 13:18:31 +0900

    feat : [010-Product-MODIFY] product & productItem modify
    product & productItem modify

--- order-api/src/main/java/com/example/cms/order/service/ProductItemService.java
+++ order-api/src/main/java/com/example/cms/order/service/ProductItemService.java
@@ -3,12 +3,14 @@ package com.example.cms.order.service;
 import com.example.cms.order.domain.model.Product;
 import com.example.cms.order.domain.model.ProductItem;
 import com.example.cms.order.domain.product.AddProductItemForm;
+import com.example.cms.order.domain.product.UpdateProductItemForm;
 import com.example.cms.order.domain.repository.ProductItemRepository;
 import com.example.cms.order.domain.repository.ProductRepository;
 import com.example.cms.order.exception.CustomException;
 import com.example.cms.order.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +29,15 @@ public class ProductItemService {
         ProductItem productItem = ProductItem.of(sellerId, form);
         product.getProductItems().add(productItem);
         return product;
+    }
+
+    @Transactional
+    public ProductItem updateProductItem(Long sellerId, UpdateProductItemForm form) {
+        ProductItem productItem = productItemRepository.findById(form.getId())
+                .filter(pi -> pi.getSellerId().equals(sellerId)).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUNT_ITEM));
+        productItem.setName(form.getName());
+        productItem.setCount(form.getCount());
+        productItem.setPrice(form.getPrice());
+        return productItem;
     }
 }
