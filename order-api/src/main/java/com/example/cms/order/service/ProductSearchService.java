9d184dd326224790eb7b69e1b917d929de5b2be8
LEE <tjddbs1412@naver.com>
2023-02-22 11:28:11 +0900

    feat : [014-Product-Cart-Check] product Cart Show & Check
    product Cart Show & Check

--- order-api/src/main/java/com/example/cms/order/service/ProductSearchService.java
+++ order-api/src/main/java/com/example/cms/order/service/ProductSearchService.java
@@ -23,7 +23,7 @@ public class ProductSearchService {
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUNT_PRODUCT));
     }
     public List<Product> getListByProductIds(List<Long> productIds) {
-        return productRepository.findAllById(productIds);
+        return productRepository.findAllByIdIn(productIds);
     }
 
 }
